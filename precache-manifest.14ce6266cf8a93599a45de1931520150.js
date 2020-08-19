034f1230857ef5018db2444b113a092cf26f6a18
Stawlie <vdowin-nikita.vdovin@yandex.ru>
2020-08-19 12:52:07 +0300

    Updates

---
+++ precache-manifest.14ce6266cf8a93599a45de1931520150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f464e39a35bdd9a0223d1f283d20a46",
+    "url": "/money-calc/index.html"
+  },
+  {
+    "revision": "688dc93b7dd7ec1e446f",
+    "url": "/money-calc/static/css/main.e0ae2d88.chunk.css"
+  },
+  {
+    "revision": "a6416410ee4e248865c7",
+    "url": "/money-calc/static/js/2.9ef5bd08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-calc/static/js/2.9ef5bd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688dc93b7dd7ec1e446f",
+    "url": "/money-calc/static/js/main.84dd49cb.chunk.js"
+  },
+  {
+    "revision": "4ef95cdeae8ea0887cfa",
+    "url": "/money-calc/static/js/runtime-main.9e566bad.js"
+  }
+]);
